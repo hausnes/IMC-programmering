c9efd0699390c48db51b3edd88c17c0833459407
Jo Bjørnar Hausnes <jb.hausnes@gmail.com>
2021-09-15 09:44:30 +0200

    Eksempler fra de første øktene.

---
+++ javascript-intro/kode.js
@@ -0,0 +1,51 @@
+console.log("Programmet fungerer.");
+
+//alert("Utskrift, alert!"); // Dette er en popup i nettleseren.
+
+// Endre innhold av typen tekst i HTML
+//document.getElementById("overskrift").innerHTML = "Hausnes";
+//document.getElementById("paragraf").innerHTML = "Ny tekst!";
+
+// Vi kan be brukeren om informasjon via et enkelt tekstfelt og bruke dette på nettsiden:
+let navn = prompt("Hva heter du?");
+document.getElementById("overskrift").innerHTML = "Hei, " + navn;
+
+// Vi kan be brukeren om en farge og bruke denne ved å endre stilark
+//let fargevalg = prompt("Hva er din favorittfarge (svar på engelsk)?");
+//document.body.style.backgroundColor = fargevalg;
+//document.getElementById("overskrift").style.color = "yellow";
+
+// Enda bedre enn eksempelet over så kan vi bruke et innebygd "verktøy" som heter input
+document.getElementById("fargevelger").onchange = function() {
+    document.body.style.background = document.getElementById("fargevelger").value; 
+}
+
+// Både å trykke på knappen og selve bildet kjører samme funksjonen ('endreBilde')
+document.getElementById("knappEndreBilde").addEventListener("click", endreBilde);
+document.getElementById("bilde").addEventListener("click", endreBilde);
+
+let aktivtBilde = "config"; // Vi tar vare på info om hvilket bilde som vises først.
+
+// Her er selve funksjonen: Basert på hvilket bilde som vises fra før så endrer vi det.
+function endreBilde() {
+    if(aktivtBilde=="config"){
+        document.getElementById("bilde").src = "bilder/laptop.png";
+        aktivtBilde = "laptop";
+    }
+    else {
+        document.getElementById("bilde").src = "bilder/config.png";
+        aktivtBilde = "config";
+    }
+}
+
+// Her gjør vi det mulig å gjøre bildet større ved å trykke på knappen
+document.getElementById("knappStørre").addEventListener("click", størreBilde);
+function størreBilde() {
+    document.getElementById("bilde").width += 10;
+}
+
+// Her gjør vi det mulig å gjøre bildet mindre ved å trykke på knappen
+document.getElementById("knappMindre").addEventListener("click", mindreBilde);
+function mindreBilde() {
+    document.getElementById("bilde").width -= 10;
+}
