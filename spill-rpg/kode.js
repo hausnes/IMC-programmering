ebaaa9a493461e399e695960de1e1693a69d23c1
Jo Bjørnar Hausnes <jb.hausnes@gmail.com>
2022-04-18 13:52:31 +0200

    RPG- animasjonar

--- spill-rpg/kode.js
+++ spill-rpg/kode.js
@@ -7,52 +7,48 @@ const helseBoss = document.getElementById("helseBoss");
 let hpHelt = 120;
 let hpBoss = 260;
 
-/*
-knappAngrip.addEventListener("click", angrip);
-
-function angrip() {
-    console.log("Angrip!");
-    //heltBilde.style.left = "350px";
-
-    //heltBilde.classList.add("animasjonHeltAngrip");
-    heltBilde.classList.toggle("animasjonHeltAngrip");
-    //heltBilde.offsetWidth;
-    //heltBilde.classList.remove("animasjonHeltAngrip");
-    
-    //heltBilde.classList.add("animasjonHeltIdle");
-
-    //heltBilde.style.animation = "heltAngrip 2s linear";
-
-    let skade = getRandomIntInclusive(3,8);
-    hpBoss -= skade;
-    helseBoss.innerHTML = "HP: " + hpBoss;
-}
-*/
-
 // Strategien for å starte animasjon på nytt: https://css-tricks.com/restart-css-animation/
 knappAngrip.addEventListener("click", function(e) {
     e.preventDefault;
 
-    // -> removing the class
-    heltBilde.classList.remove("animasjonHeltAngrip");
-
-    // -> triggering reflow /* The actual magic */
-    // without this it wouldn't work. Try uncommenting the line and the transition won't be retriggered.
-    // Oops! This won't work in strict mode. Thanks Felis Phasma!
-    // element.offsetWidth = element.offsetWidth;
-    // Do this instead:
-    void heltBilde.offsetWidth;
-
-    // -> and re-adding the class
-    heltBilde.classList.add("animasjonHeltAngrip");
-
-    let skade = getRandomIntInclusive(3,8);
+    let skade = getRandomIntInclusive(3,8); // Angrepet gjer skade mellom 3 og 8
     hpBoss -= skade;
-    helseBoss.innerHTML = "HP: " + hpBoss;
+    if (hpBoss <= 0) {
+        // Handter seier
+    } else {
+        helseBoss.innerHTML = "HP: " + hpBoss;
+        
+        // Startar animasjonen som skal indikere eit angrep
+        animer(heltBilde,"animasjonHeltAngrip");
+        /* Alternativt: 
+        heltBilde.classList.remove("animasjonHeltAngrip");
+        void heltBilde.offsetWidth;
+        heltBilde.classList.add("animasjonHeltAngrip");
+        */
+
+        // Blinkar rundt HP for å indikere at bossen mistar liv
+        animer(helseBoss,"animasjonBlinkHelse");
+
+        let skadeFraBoss = getRandomIntInclusive(5,10);
+        hpHelt -= skadeFraBoss;
+        if (hpHelt <= 0) {
+            
+        } else {
+            helseHelt.innerHTML = "HP: " + hpHelt;
+        }
+        
+        animer(helseHelt, "animasjonBlinkHelse");
+    }
 }, false);
 
 function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
-}  
+}
+
+function animer(karakter,animasjon) {
+    karakter.classList.remove(animasjon);
+    void karakter.offsetWidth;
+    karakter.classList.add(animasjon);
+}
